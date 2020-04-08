ed4bb528cb110b37845d2028148692870afa1328
Scott-Klein <Scotty.klei@gmail.com>
2020-04-08 16:53:11 +1000

    Finished logserver

--- logserver.c
+++ logserver.c
@@ -4,7 +4,27 @@
 
 int main()
 {
-	printf("Please make me useful!\n");
+    struct message buffer;
 
-	return 0;
+    printf("Generating Key\n");
+
+    key_t key;
+
+    key = ftok("logServer.o", 'S');
+    printf("Key created\nReadying Message Queue\n");
+    int queueId = msgget(key, IPC_EXCL | IPC_CREAT);
+
+    if (queueId == -1)
+    {
+        perror("Message Queue Failure: ");
+        exit(-1);
+    }
+    printf("Starting message read.\n");
+    while (1)
+    {
+        msgrcv(queueId, &buffer, sizeof(buffer), 1, 0);
+        printf("Message: %s\n", buffer.message);
+    }
+
+    return 0;
 }
