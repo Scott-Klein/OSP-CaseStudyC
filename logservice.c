8a11851c1b6b873b1567d30e65d5e024b0e454bc
Scott-Klein <Scotty.klei@gmail.com>
2020-04-08 20:51:15 +1000

    basic version of logservice

--- logservice.c
+++ logservice.c
@@ -3,15 +3,21 @@
 
 int logServiceInit()
 {
-	int id;
-
-	return id;
-
+	int id = -1; //Make no assumptions
+    key_t key = ftok("logServer.o", 'S');
+    id = msgget(key, 0666);
+    return id;
 }
 
-int logMessage(int serviceId,char*message)
+int logMessage(int serviceId, char*message)
 {
-	int rv;
+	int rv = -1;
+    struct message payload;
+    payload.type = 5;
+    strncpy(payload.message, message, MSGCHARS);
+    payload.message[MSGCHARS - 1] = '\0';
 
-	return rv;
+    msgsnd(serviceId, &message, sizeof(message), 0);
+
+    return rv;
 }
